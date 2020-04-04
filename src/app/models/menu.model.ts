88cc178ebc711a03df3e2a2e91761ebaaa2d71d1
PatrickBuhagiar <patrickbhgr@outlook.com>
2020-04-04 17:58:44 +0100

    displayed order selection table

--- src/app/models/menu.model.ts
+++ src/app/models/menu.model.ts
@@ -2,4 +2,8 @@ import {MenuItem} from './menu.item.model';
 
 export class Menu {
   items: MenuItem[];
+
+  constructor(items: MenuItem[]) {
+    this.items = items;
+  }
 }
--- src/app/pages/order-selection/order-selection.component.ts
+++ src/app/pages/order-selection/order-selection.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Menu} from '../../models/menu.model';
+import {MenuItem} from '../../models/menu.item.model';
 
 @Component({
   selector: 'app-order-selection',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderSelectionComponent implements OnInit {
 
-  constructor() { }
+  menu: Menu;
+
+  constructor() {
+  }
 
   ngOnInit(): void {
+
+    const items: MenuItem[] = [
+      new MenuItem('Pizza Margherita', 8, 'tomato and mozzarella', ['VG']),
+      new MenuItem('Chicken Nuggets', 6, '8 chicken nuggets', []),
+      new MenuItem('Greek Salad', 7, 'simple boring salad', ['GF', 'V', 'VG']),
+      new MenuItem('Chicken Breast and Veg', 12, 'baked', ['GF']),
+      new MenuItem('Ice Cream', 3, 'Vanilla only', ['GF', 'VG'])
+    ];
+    this.menu = new Menu(items);
   }
 
 }
